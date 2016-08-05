eee2ee336a68a2b34a4b2212d54c9b6f73c80bb8
Rebecca Frailich <rebecca.frailich@gmail.com>
2016-08-04 21:46:41 -0400

    messaging

--- messaging/views.py
+++ messaging/views.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from .forms import MessageForm
 
 
 class UserDetailView(generic.DetailView):
@@ -15,39 +16,23 @@ class UserDetailView(generic.DetailView):
     template_name = 'messaging/detail.html'
 
 
-def message(request, user_id):
+def convo(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    curr_user = request.user
+
+    # Will eventually support group conversations, for now we'll just get the one
+    conversation = user.conversation_set.get(pk__in=curr_user.conversation_set.all())
     form = MessageForm()
     if request.method == "POST":
         form = MessageForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
-            message.body = request.user
-            message.published_date = timezone.now()
+            message.sent_date = timezone.now()
+            message.conversation_id = conversation.id
             message.save()
-    return render(request, 'messaging/detail.html', {'form': form, 'user': user_id})  
-# user = get_object_or_404(User, pk=user_id)
-#     
-#     try:
-#     #TODO get convo with exactly desired users -- for now will just have one match
-#         conversation = user.conversation_set.get(pk__in=curr_user.conversation_set)
-#     except (KeyError, Conversation.DoesNotExist):
-#         conversation = Conversation()
-#         conversation.save()
-#         conversation.users.add(user_id, request.user_id)
-#         
-#     message = Message(body=request.POST['message'], sent_date=timezone.now(),
-#       conversation_id=conversation.id)
-#     message.save() # why won't this save?
-#     return HttpResponseRedirect(reverse('messaging:detail', args=(user.id,)))
+    return render(request, 'conversations/detail.html', {'form': form, 'user': user_id,
+      'conversation': conversation})  
 
-def convo(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    curr_user = request.user
-
-    # Will eventually support group conversations
-    conversations = user.conversation_set.filter(pk__in=curr_user.conversation_set.all())
-    context = Context({'conversations':conversations})
-    return render(request, 'conversations/detail.html', context)
 
 
 class UserIndexView(LoginRequiredMixin, generic.ListView):
